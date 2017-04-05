139d157f154a48628999c78a545965168cdab0fc
Florentin Hennecker <florentin.hennecker@gmail.com>
2017-04-05 17:34:21 +0200

    OHMYGOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOD

--- toys/rl2_mdp.py
+++ toys/rl2_mdp.py
@@ -10,11 +10,21 @@ import matplotlib.pyplot as plt
 import time
 
 
+GAMMA = 0.8
+
 def generate_n_bandits(N):
     return np.random.rand(N)
 
 def pull(bandits, n):
     return 1 if random.random() < bandits[n] else 0
+
+
+def normalized_columns_initializer(std=1.0):
+    def _initializer(shape, dtype=None, partition_info=None):
+        out = np.random.randn(*shape).astype(np.float32)
+        out *= std / np.sqrt(np.square(out).sum(axis=0, keepdims=True))
+        return tf.constant(out)
+    return _initializer
 
 
 class RL2():
@@ -28,7 +38,7 @@ class RL2():
         self.embedded_input = self.embed_input()
         
         # defining the recurrent part
-        self.cell = tf.nn.rnn_cell.GRUCell(48)
+        self.cell = tf.nn.rnn_cell.BasicLSTMCell(48)
         self.initial_state = self.cell.zero_state(self.batch_size, tf.float32)
         
         self.rnn_output, self.rnn_output_state = tf.nn.dynamic_rnn(
@@ -38,10 +48,8 @@ class RL2():
         )
 
         # fully connected and output
-        self.actions_distribution = tf.nn.softmax(
-                slim.fully_connected(self.rnn_output, self.n_arms, activation_fn=None)
-        )
-        self.value_function = tf.squeeze(slim.fully_connected(self.rnn_output, 1, activation_fn=None), 2)
+        self.actions_distribution =  slim.fully_connected(self.rnn_output, self.n_arms, activation_fn=tf.nn.softmax, weights_initializer=normalized_columns_initializer(0.01), biases_initializer=None)
+        self.value_function = tf.squeeze(slim.fully_connected(self.rnn_output, 1, activation_fn=None, weights_initializer=normalized_columns_initializer(1.0), biases_initializer=None), 2)
 
         # easy endpoint for testing step by step (removes batch size and seq len)
         self.last_actions_distribution = tf.squeeze(self.actions_distribution)
@@ -53,16 +61,19 @@ class RL2():
         self.value_loss = tf.Print(self.value_loss, [self.value_loss], "Voss: ")
         
 
-        entropy = - tf.reduce_sum(self.actions_distribution*tf.log(self.actions_distribution))
-        entropy = tf.Print(entropy, [entropy])
+        self.entropy = - tf.reduce_sum(self.actions_distribution*tf.log(self.actions_distribution+1e-7))
         self.entropy_mul = tf.placeholder(tf.float32)
-        self.loss = -tf.log(tf.reduce_sum(tf.reduce_sum(
-                self.actions_distribution * self.action_choices_OH, 2
-            ) * (self.reward-self.value_function)) +1e-10) + 0.5 * self.value_loss - entropy*self.entropy_mul # avoid log(0) with +1e-10
-        self.loss = tf.Print(self.loss, [self.loss], "Loss: ")
+        #  values_plus = tf.concat(1, [self.values, tf.zeros((self.batch_size, 1))])
+        #  self.advantage = reward + GAMMA * tf.slice(values_plus, [0, 1], [-1, -1]) - self.values
+        #  self.advantage = discount(self.reward - self.value_function
+        self.advantage = tf.placeholder(tf.float32, shape=[None, None])
+        self.policy_loss = tf.reduce_sum(-tf.log(tf.reduce_sum(self.actions_distribution * self.chosen_actions, 2)
+            +1e-7) * (self.advantage))
+        self.loss = self.policy_loss + 0.5 * self.value_loss - self.entropy*0.05#self.entropy_mul # avoid log(0) with +1e-10
+        self.loss = tf.Print(self.loss, [self.loss, self.loss/100], "Loss: ")
         #  total_loss = self.value_loss + self.loss
         self.learning_rate = tf.placeholder(tf.float32)
-        self.train_step = tf.train.RMSPropOptimizer(self.learning_rate).minimize(self.loss)
+        self.train_step = tf.train.AdamOptimizer(self.learning_rate).minimize(self.loss)
 
     def embed_input(self):
         self.action_choices = tf.cast(
@@ -72,6 +83,7 @@ class RL2():
                 tf.one_hot(self.action_choices, self.n_arms),
                 2)
         self.r_and_t = tf.slice(self.input, [0, 0, 1], [-1, -1, 2])
+        self.chosen_actions = tf.slice(tf.concat(1, (self.action_choices_OH, tf.zeros((self.batch_size, 1, 2)))), [0, 1, 0], [-1, -1, -1])
         return tf.cast(tf.concat(2, (self.action_choices_OH, self.r_and_t)), tf.float32)
 
 
@@ -88,17 +100,19 @@ def train():
     distribution_size = 10
     n_arms = 2
     n_trials = int(100e3)
-    n_episodes_per_trial = 8
+    n_episodes_per_trial = 1
     episode_length = 100
 
     nn = RL2(n_arms)
-    image = np.zeros((1000, episode_length))
-    pull_image = np.zeros((1000, episode_length))
+    image = np.zeros((500, episode_length))
+    pull_image = np.zeros((500, episode_length))
     image_line = 0
 
     start_entropy = 1.0
     end_entropy = 0.0
     end_entropy_iteration = 1
+
+    summary_writer = tf.summary.FileWriter('bonsoir3')
 
     bandits_distrib = [generate_n_bandits(n_arms) for i in range(distribution_size)]
 
@@ -114,7 +128,7 @@ def train():
         init = tf.global_variables_initializer()
         sess.run(init)
 
-        tw = tf.summary.FileWriter('/tmp/thesistrain', sess.graph)
+        tw = tf.summary.FileWriter('/tmp/thesistrain2', sess.graph)
         plt.ion()
         total_trial_rewards = []
         for t in range(n_trials):
@@ -129,6 +143,7 @@ def train():
 
             inputs = [[] for i in range(n_episodes_per_trial)]
             rewards = [[] for i in range(n_episodes_per_trial)]
+            values = [[] for i in range(n_episodes_per_trial)]
 
             for e in range(n_episodes_per_trial):
 
@@ -137,8 +152,8 @@ def train():
 
                 hidden_state = None
                 #  bandits = generate_n_bandits(n_arms)
-                bandits = random.choice(bandits_distrib)
-                random.shuffle(bandits)
+                bandits = random.choice([[0.1, 0.9], [0.9, 0.1]])
+                #  random.shuffle(bandits)
                 print(bandits)
 
                 action = random.randint(0, n_arms-1)
@@ -160,10 +175,11 @@ def train():
                     if i == 0 :
                         start_hidden_state = hidden_state
 
-                    actions_distribution, hidden_state = sess.run(
-                            [nn.last_actions_distribution, nn.rnn_output_state], 
+                    actions_distribution, hidden_state, value = sess.run(
+                            [nn.last_actions_distribution, nn.rnn_output_state, nn.value_function], 
                             feed_dict)
                     episode_distributions.append(actions_distribution)
+                    values[e].append(value[0][0])
 
                     # choosing action
                     action = np.random.choice(
@@ -179,7 +195,7 @@ def train():
                 total_trial_reward += total_episode_reward
 
             print(np.array(inputs).shape)
-            if t%40 == 0 and image_line < image.shape[0]:
+            if t%5 == 0 and image_line < image.shape[0]:
                 pulls = np.array(inputs[-1])[:,0].reshape([1, -1])
                 optimal_pulls = pulls == np.argmax(bandits)
                 quality = np.argsort(bandits)[pulls]
@@ -187,7 +203,7 @@ def train():
                 pull_image[image_line, :] = pulls
                 image_line += 1
                 start = time.time()
-            if t%200 == 0:
+            if t%20 == 0:
                 plt.figure(1)
                 plt.imshow(image, cmap='gray', interpolation='nearest')
                 plt.draw()
@@ -201,22 +217,39 @@ def train():
             entropymul = max(end_entropy, start_entropy - (start_entropy-end_entropy) * t/ end_entropy_iteration)
             print("ENTROPY :", entropymul)
             learning_rate = 1e-3
-            if t > 500:
-                learning_rate = 1e-4
-            if t > 1000:
-                learning_rate = 1e-5
+            #  if t > 500:
+                #  learning_rate = 1e-4
+            #  if t > 1000:
+                #  learning_rate = 1e-5
+            value_plus = values
+            for v in value_plus:
+                v.append(0)
+            advantages = []
+            for e in range(n_episodes_per_trial):
+                e_advantages = np.array(rewards[e]) + GAMMA * np.array(value_plus[e][1:]) - np.array(value_plus[e][:-1])
+                e_advanatages = discount(e_advantages, GAMMA)
+                advantages.append(e_advantages)
+
             feed_dict={
                 nn.batch_size : n_episodes_per_trial,
                 nn.sequence_length : episode_length,
                 nn.input : np.array(inputs),
-                nn.reward : np.array(list(map(lambda r:discount(r, 0.99), rewards))),
+                nn.reward : np.array(list(map(lambda r:discount(r, GAMMA), rewards))),
                 nn.entropy_mul : entropymul,
                 nn.learning_rate : learning_rate,
+                nn.advantage : advantages
             }
             #  if start_hidden_state is not None:
                 #  feed_dict[nn.initial_state] = start_hidden_state
-            loss, _ = sess.run([nn.loss, nn.train_step], feed_dict)
+            summary = tf.Summary()
+
+            loss, _, adv, v, vl, pl, el = sess.run([nn.loss, nn.train_step, nn.advantage, nn.value_function, nn.value_loss, nn.policy_loss, nn.entropy], feed_dict)
             print(loss, total_episode_reward)
+            summary.value.add(tag='Losses/ValueLoss', simple_value=float(vl))
+            summary.value.add(tag='Losses/PolicyLoss', simple_value=float(pl))
+            summary.value.add(tag='Losses/Entropy', simple_value=float(el))
+            summary_writer.add_summary(summary, t)
+            summary_writer.flush()
 
 
             total_trial_reward /= n_episodes_per_trial
